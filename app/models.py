ce984e1b7854520c40cd21f4d392943e21714b8d
Job Karani <jobkarani084j@gmail.com>
2022-05-07 06:12:42 +0300

    modifies profile model

--- app/models.py
+++ app/models.py
@@ -3,20 +3,6 @@ from cloudinary.models import CloudinaryField
 from django.contrib.auth.models import User
 
 # Create your models here.
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.PROTECT)
-    profile_photo = CloudinaryField('image')
-    email = models.EmailField(max_length=256, null=True)
-    phone = models.CharField(max_length=100)
-    date_joined = models.DateTimeField(auto_now_add=True)
-
-    def __str___(self):
-        return self.phone
-
-    def __str__(self):
-        return self.user.username
-
 class BankAccount(models.Model):
     user = models.ForeignKey(User, on_delete=models.PROTECT)
     account_number = models.IntegerField(null=False)
@@ -35,3 +21,17 @@ class Transactions(models.Model):
 
     def __str__(self):
         return self.user
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+    profile_photo = CloudinaryField('image')
+    email = models.EmailField(max_length=256, null=True)
+    phone = models.CharField(max_length=100)
+    account = models.ForeignKey(BankAccount, on_delete=models.PROTECT)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    def __str___(self):
+        return self.phone
+
+    def __str__(self):
+        return self.user.username
