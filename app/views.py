b313ec24f4cf33ce82d2b696ea7c14cea288c391
Job Karani <jobkarani084j@gmail.com>
2022-05-07 05:49:44 +0300

    creates profile method

--- app/views.py
+++ app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .models import *
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+@login_required(login_url="/accounts/login/")
+def profile(request):
+    current_user = request.user
+    profile = Profile.objects.filter(user_id=current_user.id).first()
+    return render(request, "all-temps/profile.html", {"profile": profile})
