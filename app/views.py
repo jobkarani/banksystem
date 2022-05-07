778921f0c31017fdd650b27f2fc4cae640c0253f
Job Karani <jobkarani084j@gmail.com>
2022-05-07 05:57:05 +0300

    creates deposit method

--- app/views.py
+++ app/views.py
@@ -8,3 +8,9 @@ def profile(request):
     current_user = request.user
     profile = Profile.objects.filter(user_id=current_user.id).first()
     return render(request, "all-temps/profile.html", {"profile": profile})
+
+@login_required(login_url="/accounts/login/")
+def deposit(request):
+    current_user = request.user
+    to = BankAccount.objects.all()
+    return render (request, "all-temps/deposit.html", {"to":to})
